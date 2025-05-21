231fcafa112646d5a3679a80fd93d45a5cb6146c
Udit <23015406+udit-001@users.noreply.github.com>
2025-05-22 04:44:16 +0530

    feat: add pwa icons

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,36 @@ export default defineConfig({
             manifest: {
                 name: "Lift",
                 short_name: "Lift",
-                description: "Track your workout routines, simply"
+                description: "Track your workout routines, simply",
+                icons: [
+                    {
+                        src: 'icons/pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'icons/pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'icons/pwa-64x64.png',
+                        sizes: '64x64',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'icons/maskable-icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable'
+                    }
+                ],
+                theme_color: '#EFF6FF',
+                background_color: '#F8FAFC',
+                display: 'standalone',
+                scope: '/',
+                start_url: '/',
+                orientation: 'portrait'
             }
         }),
     ],
