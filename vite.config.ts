1368b6c309bd3f58cffe33e8bd247baf5b40ee5a
Udit <23015406+udit-001@users.noreply.github.com>
2025-05-22 05:52:55 +0530

    feat: update pwa icons

--- vite.config.ts
+++ vite.config.ts
@@ -19,17 +19,17 @@ export default defineConfig({
                 description: "Track your workout routines, simply",
                 icons: [
                     {
-                        src: 'pwa-192x192.png',
+                        src: 'icons/pwa-192x192.png',
                         sizes: '192x192',
                         type: 'image/png'
                     },
                     {
-                        src: 'pwa-512x512.png',
+                        src: 'icons/pwa-512x512.png',
                         sizes: '512x512',
                         type: 'image/png'
                     },
                     {
-                        src: 'pwa-64x64.png',
+                        src: 'icons/pwa-64x64.png',
                         sizes: '64x64',
                         type: 'image/png',
                         purpose: 'any maskable'
